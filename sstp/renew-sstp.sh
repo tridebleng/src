de00e12780f93f7cab56f0100d454d4fd54ac1ac
tridebleng <110109155+tridebleng@users.noreply.github.com>
2022-08-25 10:56:28 +0700

    Update renew-sstp.sh

--- sstp/renew-sstp.sh
+++ sstp/renew-sstp.sh
@@ -39,7 +39,7 @@ fi
 
 PERMISSION () {
     MYIP=$(curl -sS ipv4.icanhazip.com)
-    IZIN=$(curl -sS https://raw.githubusercontent.com/tridebleng/izinvps/ipuk/ip| awk '{print $4}' | grep $MYIP)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/tridebleng/izinvps/ipuk/ip | awk '{print $4}' | grep $MYIP)
     if [ "$MYIP" = "$IZIN" ]; then
     Bloman
     else
