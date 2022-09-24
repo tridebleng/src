06c7c64ff29acd12c64e2aa15c19268413895d0b
tridebleng <110109155+tridebleng@users.noreply.github.com>
2022-09-24 10:40:14 +0700

    Update cek-ws.sh

--- xray/cek-ws.sh
+++ xray/cek-ws.sh
@@ -1,25 +1,29 @@
+#!/bin/bash
 clear
 echo -n > /tmp/other.txt
-data=( cat /etc/xray/config.json | grep '^#' | cut -d ' ' -f 2 | sort | uniq);
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo -e "\E[44;1;39m            ⇱ CECK USER LOGIN XRAY ⇲                \E[0m"
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-
+data=( `cat /etc/xray/config.json | grep '^###' | cut -d ' ' -f 2 | sort | uniq`);
+echo -e "$COLOR1
+${NC}"
+echo -e "$COLOR1
+${NC} ${COLBG1}            
+ =====VMESS USER ONLINE=====
+              ${NC} $COLOR1
+$NC"
+echo -e "$COLOR1
+${NC}"
+echo -e "$COLOR1
+${NC}"
 for akun in "${data[@]}"
-do
 if [[ -z "$akun" ]]; then
 akun="tidakada"
-fi
 echo -n > /tmp/ipvmess.txt
-data2=( cat /var/log/xray/access.log | tail -n 500 | cut -d " " -f 3 | sed 's/tcp://g' | cut -d ":" -f 1 | sort | uniq);
+data2=( `cat /var/log/xray/access.log | tail -n 500 | cut -d " " -f 3 | sed 's/tcp://g' | cut -d ":" -f 1 | sort | uniq`);
 for ip in "${data2[@]}"
-do
 jum=$(cat /var/log/xray/access.log | grep -w "$akun" | tail -n 500 | cut -d " " -f 3 | sed 's/tcp://g' | cut -d ":" -f 1 | grep -w "$ip" | sort | uniq)
 if [[ "$jum" = "$ip" ]]; then
 echo "$jum" >> /tmp/ipvmess.txt
 else
 echo "$ip" >> /tmp/other.txt
-fi
 jum2=$(cat /tmp/ipvmess.txt)
 sed -i "/$jum2/d" /tmp/other.txt > /dev/null 2>&1
 done
@@ -28,14 +32,25 @@ if [[ -z "$jum" ]]; then
 echo > /dev/null
 else
 jum2=$(cat /tmp/ipvmess.txt | nl)
-echo "user : $akun";
-echo "$jum2";
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-fi
+echo -e "$COLOR1
+${NC}   user : $akun";
+echo -e "$COLOR1
+${NC}   $jum2";
 rm -rf /tmp/ipvmess.txt
 done
 rm -rf /tmp/other.txt
-echo > /var/log/xray/access.log
+echo -e "$COLOR1
+${NC}" 
+echo -e "$COLOR1
+ BY 
+${NC}"
+echo -e "$COLOR1
+${NC}              
+ WWW.DOTYCAT.COM 
+                $COLOR1
+$NC"
+echo -e "$COLOR1
+${NC}" 
 echo ""
-read -n 1 -s -r -p "Press any key to back on menu"
-menu
+read -n 1 -s -r -p "   Press any key to back on menu"
+menu-vmess
